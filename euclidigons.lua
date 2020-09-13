dfe59b1b91770f3b891fb35411250123b9ffd8ed
Kenji Garland <kenjigarland@gmail.com>
2020-09-14 04:44:26 +0700

    Remove/add shapes with K1+K2/K3

--- euclidigons.lua
+++ euclidigons.lua
@@ -18,6 +18,46 @@ scale = musicutil.generate_scale(36, 'minor pentatonic', 1)
 alt = false
 shift = false
 
+function get_next_shape(direction)
+	local best_distance = math.huge
+	local nearest_shape = edit_shape
+	for s = 1, #shapes do
+		local shape = shapes[s]
+		if shape ~= edit_shape then
+			local distance = (shape.x - edit_shape.x) * direction
+			if distance > 0 and distance < best_distance then
+				best_distance = distance
+				print(best_distance)
+				nearest_shape = shape
+			end
+		end
+	end
+	return nearest_shape
+end
+
+function delete_shape()
+	local found = false
+	for s = 1, #shapes do
+		if shapes[s] == edit_shape then
+			found = true
+		end
+		if found then
+			shapes[s] = shapes[s + 1]
+		end
+	end
+	if #shapes > 0 then
+		edit_shape = get_next_shape(-1)
+	else
+		edit_shape = nil
+	end
+end
+
+function insert_shape()
+	-- TODO: randomize parameters?
+	edit_shape = Shape.new(1, 3, 30, 63.5, tau / 200)
+	table.insert(shapes, edit_shape)
+end
+
 function handle_strike(shape, side)
 	-- crow.ii.tt.script(2)
 	-- TODO: throttle notes -- PolyPerc can crash SC when asked to create too many synths
@@ -68,23 +108,27 @@ function redraw()
 			shapes[s]:draw_lines()
 		end
 	end
-	edit_shape:draw_lines(true)
+	if edit_shape ~= nil then
+		edit_shape:draw_lines(true)
+	end
 	for s = 1, #shapes do
 		if shapes[s] ~= edit_shape then
 			shapes[s]:draw_points()
 		end
 	end
-	edit_shape:draw_points(true)
-	if shift or alt then
-		screen.circle(edit_shape.x, y_center, 6)
-		screen.level(0)
-		screen.fill()
-		screen.move(edit_shape.x, y_center + 2.5)
-		screen.level(15)
-		if shift then
-			screen.text_center(edit_shape.n)
-		elseif alt then
-			screen.text_center(edit_shape.note_name)
+	if edit_shape ~= nil then
+		edit_shape:draw_points(true)
+		if shift or alt then
+			screen.circle(edit_shape.x, y_center, 6)
+			screen.level(0)
+			screen.fill()
+			screen.move(edit_shape.x, y_center + 2.5)
+			screen.level(15)
+			if shift then
+				screen.text_center(edit_shape.n)
+			elseif alt then
+				screen.text_center(edit_shape.note_name)
+			end
 		end
 	end
 	screen.update()
@@ -94,10 +138,22 @@ function key(n, z)
 	if n == 1 then
 		alt = z == 1
 	elseif n == 2 then
-		shift = z == 1
+		if alt then
+			if z == 1 then
+				delete_shape()
+			else
+				shift = false
+			end
+		else
+			shift = z == 1
+		end
 	elseif n == 3 then
 		if z == 1 then
-			edit_shape.mute = not edit_shape.mute
+			if alt then
+				insert_shape()
+			elseif edit_shape ~= nil then
+				edit_shape.mute = not edit_shape.mute
+			end
 		end
 	end
 end
@@ -105,41 +161,32 @@ end
 function enc(n, d)
 	if n == 1 then
 		-- select shape to edit
-		local best_distance = math.huge
-		local nearest_shape = edit_shape
-		for s = 1, #shapes do
-			local shape = shapes[s]
-			if shape ~= edit_shape then
-				local distance = (shape.x - edit_shape.x) * d
-				if distance > 0 and distance < best_distance then
-					best_distance = distance
-					print(best_distance)
-					nearest_shape = shape
-				end
+		edit_shape = get_next_shape(d)
+	elseif n == 2 then
+		if edit_shape ~= nil then
+			if shift then
+				-- set rotation rate
+				edit_shape.rate = edit_shape.rate + d * 0.0015
+			elseif alt then
+				-- set note
+				edit_shape.note = edit_shape.note + d
+			else
+				-- set position
+				edit_shape.delta_x = edit_shape.delta_x + d
 			end
 		end
-		edit_shape = nearest_shape
-	elseif n == 2 then
-		if shift then
-			-- set rotation rate
-			edit_shape.rate = edit_shape.rate + d * 0.0015
-		elseif alt then
-			-- set note
-			edit_shape.note = edit_shape.note + d
-		else
-			-- set position
-			edit_shape.delta_x = edit_shape.delta_x + d
-		end
 	elseif n == 3 then
-		if shift then
-			-- set number of sides
-			edit_shape.n = util.clamp(edit_shape.n + d, 1, 9)
-		elseif alt then
-			-- set octave
-			edit_shape.note = edit_shape.note + d * #scale
-		else
-			-- set size
-			edit_shape.r = math.max(edit_shape.r + d, 1)
+		if edit_shape ~= nil then
+			if shift then
+				-- set number of sides
+				edit_shape.n = util.clamp(edit_shape.n + d, 1, 9)
+			elseif alt then
+				-- set octave
+				edit_shape.note = edit_shape.note + d * #scale
+			else
+				-- set size
+				edit_shape.r = math.max(edit_shape.r + d, 1)
+			end
 		end
 	end
 end
